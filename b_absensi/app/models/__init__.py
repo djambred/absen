8aacdf44c32bd85927b8fcc2db0c24323916ebbf
djambred <djambred@gmail.com>
2026-01-14 13:19:22 +0700

    added form asign

--- b_absensi/app/models/__init__.py
+++ b_absensi/app/models/__init__.py
@@ -1,8 +1,9 @@
 from .user import User, UserRole, Position, PositionCategory, user_positions
-from .absensi import Attendance, AttendanceStatus, Leave, LeaveStatus, LeaveType, LeaveCategory, LeaveQuota
+from .absensi import Attendance, AttendanceStatus, Leave, LeaveStatus, LeaveType, LeaveCategory, LeaveQuota, Task, TaskStatus
 
 __all__ = [
     'User', 'UserRole', 'Position', 'PositionCategory', 'user_positions',
     'Attendance', 'AttendanceStatus', 
-    'Leave', 'LeaveStatus', 'LeaveType', 'LeaveCategory', 'LeaveQuota'
+    'Leave', 'LeaveStatus', 'LeaveType', 'LeaveCategory', 'LeaveQuota',
+    'Task', 'TaskStatus'
 ]
